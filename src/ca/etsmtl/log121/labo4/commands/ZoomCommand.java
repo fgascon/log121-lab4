14643416353b584e5e622a69cef0de029bc7a568
Frédéric Gascon <frederic.gascon@gmail.com>
2013-11-22 14:18:48 -0500

    controller base code

---
+++ src/ca/etsmtl/log121/labo4/commands/ZoomCommand.java
@@ -0,0 +1,29 @@
+package ca.etsmtl.log121.labo4.commands;
+
+
+/**
+ * 
+ */
+public class ZoomCommand implements Command {
+	
+	/**
+	 * 
+	 */
+	public ZoomCommand() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void execute() {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void unexecute() {
+		
+	}
+}
