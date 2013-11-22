54b9c55d959790840bbbfa84317bd43a4941c152
Frédéric Gascon <frederic.gascon@gmail.com>
2013-11-22 13:47:39 -0500

    CommandManager cleanup

--- src/ca/etsmtl/log121/labo4/commands/CommandManager.java
+++ src/ca/etsmtl/log121/labo4/commands/CommandManager.java
@@ -2,55 +2,39 @@ package ca.etsmtl.log121.labo4.commands;
 
 
 /**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
+ * 
  */
-
 public class CommandManager
 {
+	
+	private static final CommandManager instance = new CommandManager();
+	
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
+	 * 
+	 */
+	public static CommandManager getInstance() {
+		return instance;
+	}
+	
+	/**
+	 * 
 	 */
 	public CommandManager(){
-		super();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
-	 */
-	
-	public static CommandManager getInstance() {
-		// TODO : to implement
-		return new CommandManager();	
+		
 	}
 	
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
+	 * 
 	 */
-	
 	public void undo() {
 		// TODO : to implement	
 	}
 	
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
+	 * 
 	 */
-	
 	public void redo() {
 		// TODO : to implement	
 	}
-	
 }
 
