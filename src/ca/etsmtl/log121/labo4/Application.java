d74384e7458c9d0e01fcd6211f5b9e28e4f3b6d1
DrTroll <gabriel.sthilairev4si972@gmail.com>
2013-12-05 21:28:25 -0500

    titre labo4

--- src/ca/etsmtl/log121/labo4/Application.java
+++ src/ca/etsmtl/log121/labo4/Application.java
@@ -1,3 +1,15 @@
+/******************************************************
+Cours:  LOG121
+Projet: Laboratoire 4
+Nom du fichier: Application.java
+Date créé: 2013-11-22
+ *******************************************************
+Historique des modifications
+ *******************************************************
+ *@author Shaun-David Sauro, Gabriel St-Hilaire, Frédéric Gascon
+ *@date 2013-11-22
+ *******************************************************/
+
 package ca.etsmtl.log121.labo4;
 
 import java.io.File;
