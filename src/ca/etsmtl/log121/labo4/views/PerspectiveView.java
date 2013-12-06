2162e0410a587b388395e4c6b527650efddb1174
DrTroll <gabriel.sthilairev4si972@gmail.com>
2013-12-05 22:15:49 -0500

    titre2

--- src/ca/etsmtl/log121/labo4/views/PerspectiveView.java
+++ src/ca/etsmtl/log121/labo4/views/PerspectiveView.java
@@ -1,3 +1,14 @@
+/******************************************************
+Cours:  LOG121
+Projet: Laboratoire 4
+Nom du fichier: PerspectiveView.java
+Date créé: 2013-11-22
+ *******************************************************
+Historique des modifications
+ *******************************************************
+ *@author Shaun-David Sauro, Gabriel St-Hilaire, Frédéric Gascon
+ *@date 2013-11-22
+ *******************************************************/
 package ca.etsmtl.log121.labo4.views;
 
 import java.awt.Graphics;
